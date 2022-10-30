c2c08121d3ba8724d89f09f3ea5a2ad12757dcce
average-alien <allentang113@gmail.com>
2022-10-30 13:46:07 -0700

    adds some comments

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('./public'))
 
+// Home page -- the only page
 app.get("/", (req, res) => {
     res.render('index')
 })
 
+// Route for serving the model
 app.get("/model", (req, res) => {
     const options = {
         root: "./public"
